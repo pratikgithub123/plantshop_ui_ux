b66ec8e43f75cbdc5b9f183494e75e15d13e8800
pratikgithub123 <dangipratik19@gmail.com>
2024-07-15 17:04:54 +0545

    usermodel

---
+++ backend/model/userModel.js
@@ -0,0 +1,38 @@
+
+const mongoose = require('mongoose');
+
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    phonenum: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    cart: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cart'
+    }
+    
+});
+
+const Users = mongoose.model('users', userSchema);
+module.exports = Users;
