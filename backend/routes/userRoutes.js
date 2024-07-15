c8648f4374c5af1bc3e39ccf31786314f4cf6c96
pratikgithub123 <dangipratik19@gmail.com>
2024-07-15 16:58:34 +0545

    userroute

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,20 @@
+
+//import router from express
+const router = require('express').Router();
+const userController = require('../controllers/userControllers.js');
+//routes for the user
+router.post('/create',userController.createUser)
+
+router.post('/login' ,userController.loginUser)
+
+router.get('/get_users',  userController.getUsers); 
+
+router.delete('/delete_user/:id',  userController.deleteUser);
+
+
+
+
+
+
+module.exports = router;  
+
