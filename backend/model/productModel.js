3890d1773d36f972c700c9f9a0a83fa7a7ea0ab1
pratikgithub123 <dangipratik19@gmail.com>
2024-07-15 17:05:47 +0545

    productmodel

---
+++ backend/model/productModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    productName :{
+        type : String,
+        required : true,
+        trim : true,
+    },
+    productPrice :{
+        type : Number,
+        required : true,
+        trim : true,
+    },
+    productDescription :{
+        type : String,
+        required : true,
+        trim : true,
+    },
+    productCategory :{
+        type : String,
+        required : true,
+        trim : true,
+    },
+    productImageUrl : {
+        type : String,
+        required : true,
+    },
+    createdAt :{
+        type : Date,
+        default : Date.now(),
+    },
+   productFeatured: {
+        type: Boolean,
+        default: false,
+    },
+
+})
+
+const Products = mongoose.model('products', productSchema);
+module.exports = Products;
