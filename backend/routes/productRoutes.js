946e67d2040af88b5fdb967ddd73e296f62ddf5e
pratikgithub123 <dangipratik19@gmail.com>
2024-07-15 17:02:32 +0545

    productroutes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const productController = require('../controllers/productController');
+const { authGuard, authGuardAdmin } = require('../middleware/authGuard');
+
+// Create the product
+router.post('/create_product', productController.createProduct);
+
+// Get all products
+router.get('/get_products', productController.getProducts);
+
+// Get single product
+router.get('/get_product/:id', productController.getSingleProduct);
+
+// Update the product
+router.put('/update_product/:id', productController.updateProduct);
+
+// Delete the product
+router.delete('/delete_product/:id', productController.deleteProduct);
+
+// Search the products
+router.get('/search/:products', productController.searchProducts);
+
+//featured products
+router.get('/featured_products', productController.getFeaturedProducts);
+
+module.exports = router;
