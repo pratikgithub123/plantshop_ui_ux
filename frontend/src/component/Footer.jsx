4a8d0cd1fab3d565d4d19008fd64877883a4806b
pratikgithub123 <dangipratik19@gmail.com>
2024-07-16 15:04:40 +0545

    comp

---
+++ frontend/src/component/Footer.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './Footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p>&copy; Order the fresh fruits at your door steps !  
+          2024 Pratik Fruit App All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/component/Navbar.jsx
@@ -0,0 +1,75 @@
+// Navbar.js
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import cart from '../assets/cart.png';
+import logo from '../assets/logo.png';
+import './Navbar.css';
+
+const Navbar = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate('/login');
+  };
+
+  return (
+    <div className='navbar'>
+      <Link to="/home">
+        <img src={logo} alt="logo" className='logo' />
+      </Link>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        
+        {user ? (
+          <>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            {user.isAdmin && (
+              <li className="dropdown">
+                <span className="dropbtn"></span>
+                <div className="dropdown-content">
+                  <Link to="/admin/dashboard">Admin Dashboard</Link>
+                </div>
+              </li>
+            )}
+            <li onClick={handleLogout}>
+              <FontAwesomeIcon icon={faSignOutAlt} />
+            </li>
+            <li>
+              {user.isAdmin ? (
+                <span>Welcome Admin, {user.fullname}</span>
+              ) : (
+                <span>Welcome, {user.fullname}</span>
+              )}
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Registration</Link>
+            </li>
+          </>
+        )}
+      </ul>
+      <Link to="/cart">
+        <img src={cart} alt="cart" className='cart' />
+        
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
