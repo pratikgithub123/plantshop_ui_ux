b3d46e872b2f7ce7ea334305ea34c0650ace6040
pratikgithub123 <dangipratik19@gmail.com>
2024-07-17 20:29:10 +0545

    cartmodel

--- backend/model/cartModel.js
+++ backend/model/cartModel.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
 
 const cartItemSchema = new mongoose.Schema({
-    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-    quantity: { type: Number, default: 1 }
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',  
+        required: true
+    },
+    quantity: {
+        type: Number,
+        default: 1
+    }
 });
 
 const cartSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',  
+        required: true
+    },
     items: [cartItemSchema]
 });
 
